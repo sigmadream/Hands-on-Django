889e05b83ab5f81b22a3d9efc65ad279e9171957
Sangkon, Han <sigmadream@gmail.com>
2023-12-17 04:41:27 +0900

    feat: Django tutorial part4~6

--- mystie/polls/views.py
+++ mystie/polls/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import HttpResponse
-from django.http import Http404
+from django.shortcuts import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -13,7 +16,44 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the reustls of questions {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = selected_choice.votes + 1
+        selected_choice.save()
+        print(f'{selected_choice.votes}')
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
